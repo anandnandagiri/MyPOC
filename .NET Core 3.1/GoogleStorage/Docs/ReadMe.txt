7e446fb18a14e8bb42e2404105b919a28200fdc1
VAapps <VAapps.in@gmail.com>
2019-12-15 20:31:45 +0530

    Adding new project GoogleStorage

---
+++ .NET Core 3.1/GoogleStorage/Docs/ReadMe.txt
@@ -0,0 +1,15 @@
+Caution
+	This will modify environment variable GOOGLE_APPLICATION_CREDENTIALS
+
+
+Create Storage Admin Service Account (Sample JSON in docs)
+	Goto https://console.cloud.google.com/
+	Navigate to IAM & admin > Service Account
+	In Screen
+		Click On Create Service Account
+		Make Sure you Provided Storage Admin
+		Create Key and download JSON
+
+
+Install-Package Google.Cloud.Storage.V1 -Version 2.4.0
+
---
+++ .NET Core 3.1/GoogleStorage/GoogleStorage/Program.cs
@@ -0,0 +1,74 @@
+﻿using Google.Cloud.Storage.V1;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace GoogleStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exepath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            string jsonpath = Path.Combine(exepath, "disco-bedrock-238412-18955f7057ef.json");
+            string projectID = "disco-bedrock-238412";
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", jsonpath);
+            Console.WriteLine(Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS"));
+            Console.WriteLine("Storage List");
+
+            var storage = StorageClient.Create();
+
+
+            string downloadpath = exepath + @"\downloads\";
+            Directory.CreateDirectory(downloadpath);
+            foreach (var bucket in storage.ListBuckets(projectID))
+            {                                                 
+
+                if (!bucket.Name.ToLower().Contains("artifacts"))
+                {
+                    string bucketpath = downloadpath + bucket.Name ;
+                    Directory.CreateDirectory(bucketpath);
+                    Console.WriteLine("");
+                    Console.WriteLine(bucket.Name);
+                    string storageobjpath = bucketpath;
+                    foreach (var storageObject in storage.ListObjects(bucket.Name, ""))
+                    {     
+                        string file = Path.GetFileName(storageObject.Name);                                     
+                        if (!string.IsNullOrEmpty(file))
+                        {
+
+                            string fileName = bucketpath + @"\" + storageObject.Name.Replace('/','\\');
+                            string dirName = Path.GetDirectoryName(fileName);
+                            Directory.CreateDirectory(dirName);
+                            Console.WriteLine($"{fileName}");
+                            using (var outputFile = File.OpenWrite(fileName))
+                            {
+                                storage.DownloadObject(bucket.Name, storageObject.Name, outputFile);
+                            }           
+                           //Console.WriteLine($"{Path.GetFileName(storageObject.Name)}  -> {dirName}");
+                        }
+                        else
+                        {
+                            Directory.CreateDirectory(bucketpath + @"\" + storageObject.Name + @"\");
+                            storageobjpath = bucketpath + @"\" + storageObject.Name + @"\";
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private void DownloadObject(StorageClient storage,string bucketName, string objectName, string localPath = null)
+        {                                             
+            localPath = localPath ?? Path.GetFileName(objectName);
+            using (var outputFile = File.OpenWrite(localPath))
+            {
+                storage.DownloadObject(bucketName, objectName, outputFile);
+            }
+            Console.WriteLine($"downloaded {objectName} to {localPath}.");
+        }
+    }
+}
