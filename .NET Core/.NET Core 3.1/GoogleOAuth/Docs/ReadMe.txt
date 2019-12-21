a8e45197d37f7efde5d674d3e0776eef7559fa22
VAapps <VAapps.in@gmail.com>
2019-12-21 17:39:48 +0530

    .NET Core ReStructure with versions

---
+++ .NET Core/.NET Core 3.1/GoogleOAuth/Docs/ReadMe.txt
@@ -0,0 +1,19 @@
+External Links
+	https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+	https://github.com/GoogleCloudPlatform/dotnet-docs-samples
+	https://docs.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-3.1
+	https://docs.microsoft.com/en-us/aspnet/core/security/authentication/scaffold-identity?view=aspnetcore-3.1&tabs=visual-studio
+
+Adding Nuget Packages
+	Install-Package Microsoft.AspNetCore.Authentication.Google -Version 3.1.0	
+	Install-Package Microsoft.EntityFrameworkCore.SqlServer -Version 3.1.0
+	Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore -Version 3.1.0
+	Install-Package Microsoft.EntityFrameworkCore.Sqlite -Version 3.1.0
+	Install-Package Microsoft.AspNetCore.Identity.UI -Version 3.1.0 [This will add automatically when you scaffold identity]
+
+
+	https://localhost:5001/signin-google
+	Install-Package Microsoft.AspNetCore.Authentication.Google -Version 2.2.0
+
+
+	
---
+++ .NET Core/.NET Core 3.1/GoogleOAuth/GoogleOAuth/Startup.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using GoogleOAuth.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleOAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+                            {
+                                options.SignIn.RequireConfirmedAccount = true;
+                                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,-._@+/ ()";
+                            })
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+                                                                                                    
+
+            services.AddAuthentication()
+                    .AddGoogle(options =>
+                        {
+                            IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+                            options.ClientId = googleAuthNSection["ClientId"];
+                            options.ClientSecret = googleAuthNSection["ClientSecret"];                                         
+                        });
+
+            //services.AddRazorPages();
+            //services.AddControllersWithViews();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            //app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //endpoints.MapRazorPages();
+            //endpoints.MapControllers();
+            //endpoints.MapDefaultControllerRoute();
+            //endpoints.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
+            //    endpoints.MapAreaControllerRoute(
+            //        name: "identityarea",
+            //        areaName: "identity",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            //});
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
