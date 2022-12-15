a0060d8a1bb0f477fdfc7877e2f19dcec1726514
anand <pavan.anand.n@gmail.com>
2022-12-15 16:14:40 +0530

    MyPOC - HTML Minifer Initial Draft

---
+++ .NET Core/.NET 6/HTMLMinifer/HTMLMinifer/CSS.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HTMLMinifer
+{
+    public static class CSS
+    {
+        public static void Init()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Minifier for CSS files...");
+            Console.WriteLine("");
+            Console.WriteLine($"Current Working Directory.. {Directory.GetCurrentDirectory()}");
+            Console.WriteLine("");
+            int btfi = 0; int affi = 0;
+            foreach (var f in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.css", SearchOption.AllDirectories))
+            {
+                //Console.WriteLine(f.ToString());
+                StringBuilder Sb = new StringBuilder();
+                Sb.Append(System.IO.File.ReadAllText(f.ToString(),Encoding.UTF8));
+                FileInfo fi = new FileInfo(f.ToString());
+                int bfi = Convert.ToInt32($"{fi.Length / 1024}");
+
+                //Remove New Line
+                //Sb.Replace(Environment.NewLine, "");
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"[\r\n]", " "));            
+                //Sb.Replace("\\s", "");
+                ////Remove Tab
+                //Sb.Replace("\\t", "");
+                //Sb.Replace("\\n", "");
+
+                //Remove Comments                
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"/\*.*?\*/", ""));
+
+                ////Remove Spaces
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"{\s+", "{"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"\s+{", "{"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"}\s+", "}"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"\s+}", "}"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @":\s+", ":"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @";\s+", ";"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"\s+;", ";"));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"\s+,", ","));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @",\s+", ","));
+                Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"{\s+}", "{}"));
+
+                File.WriteAllText($"{f.ToString()}", Sb.ToString());
+                fi = new FileInfo($"{f.ToString()}");
+                int afi = Convert.ToInt32($"{fi.Length / 1024}");
+                Console.WriteLine($"{fi.Name.ToString()} ... {bfi} KB: {afi} KB");
+                btfi = btfi + bfi;
+                affi = affi + afi;
+                //Console.WriteLine(Regex.Replace(Sb.ToString(), @">\s+<", "><"));   
+            }
+            Console.WriteLine("");
+            Console.WriteLine($" Total Before File Size {btfi} KB");
+            Console.WriteLine($" Total After File Size {affi} KB");
+            Console.WriteLine($" Total Minified {btfi - affi} KB");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+    }
+}
--- .NET Core/.NET 6/HTMLMinifer/HTMLMinifer/Program.cs
+++ .NET Core/.NET 6/HTMLMinifer/HTMLMinifer/Program.cs
@@ -1,46 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Text;
-using System.Text.RegularExpressions;
 
-Console.WriteLine("Minifier for HTML files...");
-Console.WriteLine("");
-Console.WriteLine($"Current Working Directory.. {Directory.GetCurrentDirectory()}");
-Console.WriteLine("");
-int btfi = 0; int affi = 0;
-foreach(var f in Directory.GetFiles(Directory.GetCurrentDirectory(),"*.html",SearchOption.AllDirectories))
-{
-    //Console.WriteLine(f.ToString());
-    StringBuilder Sb = new StringBuilder();
-    Sb.Append(System.IO.File.ReadAllText(f.ToString()));
-    FileInfo fi = new FileInfo(f.ToString());
-    int bfi = Convert.ToInt32($"{fi.Length / 1024}");
 
-    //Remove Spaces between HTML Elements
-    Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @">\s+<", "><"));
- 
-    //Remove Comments
-    Sb.Replace(Sb.ToString(), Regex.Replace(Sb.ToString(), @"<!--.*?-->", ""));
+using HTMLMinifer;
+using System.ComponentModel.Design.Serialization;
 
-    //Remove Tab
-    Sb.Replace("\t", "");
+HTML.Init();
 
-    //Remove New Line
-    Sb.Replace(Environment.NewLine, "");
+CSS.Init();
 
-  
-    File.WriteAllText($"{f.ToString()}.min", Sb.ToString());
-    fi= new FileInfo($"{f.ToString()}.min");
-    int afi = Convert.ToInt32($"{fi.Length / 1024}");
-    Console.WriteLine($"{fi.Name.ToString()} ... {bfi} KB: {afi} KB");
-    btfi = btfi + bfi;
-    affi = affi + afi;
-    //Console.WriteLine(Regex.Replace(Sb.ToString(), @">\s+<", "><"));   
-}
-Console.WriteLine("");
-Console.WriteLine($" Total Before File Size {btfi} KB");
-Console.WriteLine($" Total After File Size {affi} KB");
-Console.WriteLine($" Total Minified {btfi-affi} KB");
-
-Console.WriteLine("");
-Console.WriteLine("");
 Console.WriteLine(""); Console.WriteLine("");
